03a8c08990666fd4c030a116540dc1baa6ae1e82
frederikkerosenbom <frro0004@stud.ek.dk>
2025-11-03 16:24:42 +0100

    animation på infoboxes done

--- emergency-main/js/main.js
+++ emergency-main/js/main.js
@@ -69,6 +69,9 @@ document
 
 function headsetInfo() {
   console.log("headsetInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent =
     "Når beatet overdøver virkeligheden";
   document.querySelector(".info-text > article > p").textContent =
@@ -84,6 +87,9 @@ document.querySelector("#hotspotHue").addEventListener("click", hueInfo);
 
 function hueInfo() {
   console.log("hueInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Den glemte helt";
   document.querySelector(".info-text > article > p").textContent =
     " “Jeg skal jo ikke så langt, den ødelægger min frisure, den ligger der hjemme” - sagde alle, der endte på skadestuen. Men hey - frisuren sad jo perfekt, indtil du ramte fortovskanten. Uanset om du kører på rulleskøjter, cykel eller el-løbehjul, er hjelmen stadig den mest undervurderede superhelt.";
@@ -98,6 +104,9 @@ document.querySelector("#hotspotSko").addEventListener("click", skoInfo);
 
 function skoInfo() {
   console.log("skoInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Den skjulte fjende";
   document.querySelector(".info-text > article > p").textContent =
     "Du ville bare lige rulle lidt - men ét løst snørebånd, og pludselig laver du parkour midt på cykelstien. Lyder fjollet? Jep. Men præcis sådan starter mange ulykker - også på el-løbehjul og cykel, hvor udstyr og manglende fokus sender folk i asfalten.";
@@ -111,6 +120,9 @@ document.querySelector("#hotspotPhone").addEventListener("click", phoneInfo);
 
 function phoneInfo() {
   console.log("phoneInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent =
     "Bare lige et hurtigt kig…";
   document.querySelector(".info-text > article > p").textContent =
@@ -119,4 +131,25 @@ function phoneInfo() {
     "<h3>Effektivitet</h3><p>lorem ipsum4</p>";
   document.querySelector("#requirement").innerHTML =
     "<h3>Styrke</h3><p>lorem ipsum4</p>";
+}
+
+function animateBoxes() {
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadeIn");
+
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#requirement").classList.add("fadeIn");
+
+  document
+    .querySelector("#requirement")
+    .addEventListener("animationend", cleanUp);
+}
+
+function cleanUp() {
+  console.log("cleanUp");
+  document
+    .querySelector("#requirement")
+    .removeEventListener("animationend", cleanUp);
+  document.querySelector("#efficiency").classList.remove("fadeIn");
+  document.querySelector("#requirement").classList.remove("fadeIn");
 }
